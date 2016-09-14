273d4b85fbfce430c50726972b9c4003d2fb45bf
Patrik Bergman <patrik.bergman@transmode.com>
2016-09-14 13:58:05 +0200

    Removed tnm folder

--- tnm/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-#-e  Exit immediately if a command exits with a non-zero status.
-set -e
-
-exec service mysql start 
-exec service rabbitmq-server start
-exec service tnmserver start
